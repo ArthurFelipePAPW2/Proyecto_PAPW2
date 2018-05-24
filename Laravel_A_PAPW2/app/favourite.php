0b13155d4969a925aeef942eca3f3b22119916f7
ArthurFelipePAPW2 <36238187+ArthurFelipePAPW2@users.noreply.github.com>
2018-05-24 15:09:43 -0500

    Actualizacion 24.5.18
    *se agregó el softdelete
    
    QUE FACIL ES PAPW AAAAAAA

--- Laravel_A_PAPW2/app/favourite.php
+++ Laravel_A_PAPW2/app/favourite.php
@@ -3,13 +3,18 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class favourite extends Model
 {
+	use SoftDeletes;
+
     protected $primaryKey = 'id-favourite';
     protected $fillable = ['id-videogame','id-user'];
 
     public function Videogame(){
         return $this->belongsTo(videogame::Class,'id-videogame');
     }
+
+    protected $dates = ['deleted_at'];
 }
--- Laravel_A_PAPW2/app/useful.php
+++ Laravel_A_PAPW2/app/useful.php
@@ -3,14 +3,19 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use App\review;
 
 class useful extends Model
 {
+	use SoftDeletes;
+
     protected $primaryKey = 'id-useful';
     protected $fillable = ['id-user', 'id-review'];
 
     public function Review(){
         return $this->belongsTo(review::Class,'id-review');
     }
+
+    protected $dates = ['deleted_at'];
 }
